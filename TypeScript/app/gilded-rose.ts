9ee43a4a34ab7cd8392add89eeffd87fc50fd06e
Binu Varghese <binsdevapps@gmail.com>
2023-11-07 07:23:21 +0000

    Update gilded-rose.ts

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -17,6 +17,69 @@ export class GildedRose {
     this.items = items;
   }
 
+  private static isLegendary(item: Item): boolean {
+    return (item.name || "").toLowerCase() === "sulfuras";
+  }
+
+  private static isAgedBrie(item: Item): boolean {
+    return (item.name || "").toLowerCase() === "aged brie";
+  }
+
+  private static isBackstagePass(item: Item): boolean {
+    return (item.name || "").toLowerCase() === "backstage passes";
+  }
+
+  private static isConjured(item: Item): boolean {
+    return  (item.name || "").toLowerCase().indexOf('conjured') === 0;
+  }
+
+  private static updateQualityForAgedBrie(item: Item): void {
+    if (item.quality < 50) {
+      item.quality += 1;
+    }
+  }
+
+  private static updateQualityForBackstagePass(item: Item): void {
+    if (item.sellIn < 0) {
+      item.quality = 0;
+    } else if (item.sellIn < 5) {
+      item.quality += 3;
+    } else if (item.sellIn < 10) {
+      item.quality += 2;
+    } else {
+      item.quality += 1;
+    }
+    item.quality = Math.min(item.quality, 50);
+  }
+
+  private static updateQualityForConjured(item: Item): void {
+    item.quality -= item.sellIn < 0 ? 4 : 2;
+    item.quality = Math.max(item.quality, 0);
+  }
+
+  private static updateQualityForRegularItem(item: Item): void {
+    item.quality -= item.sellIn < 0 ? 2 : 1;
+    item.quality = Math.max(item.quality, 0);
+  }
+
+  updateQuality() {
+    for (let item of this.items) {
+      if (GildedRose.isLegendary(item)) continue;
+
+      item.sellIn -= 1;
+
+      if (GildedRose.isAgedBrie(item)) {
+        GildedRose.updateQualityForAgedBrie(item);
+      } else if (GildedRose.isBackstagePass(item)) {
+        GildedRose.updateQualityForBackstagePass(item);
+      } else if (GildedRose.isConjured(item)) {
+        GildedRose.updateQualityForConjured(item);
+      } else {
+        GildedRose.updateQualityForRegularItem(item);
+      }
+    }
+  }
+/* Old method
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
@@ -65,5 +128,5 @@ export class GildedRose {
     }
 
     return this.items;
-  }
+  }  */
 }
